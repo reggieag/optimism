b979d38e91a3ed1389596eb2806f559f3eb6a5b6
Karl Floersch <karl@karlfloersch.com>
2021-06-04 20:24:33 -0400

    fix: estimate gas when pending tx in mempool

--- packages/batch-submitter/src/batch-submitter/tx-batch-submitter.ts
+++ packages/batch-submitter/src/batch-submitter/tx-batch-submitter.ts
@@ -140,6 +140,10 @@ export class TransactionBatchSubmitter extends BatchSubmitter {
 
       if (!this.disableQueueBatchAppend) {
         const nonce = await this.signer.getTransactionCount()
+        // Generate the transaction we will repeatedly submit
+        const tx = await this.chainContract.populateTransaction.appendQueueBatch(
+          99999999
+        )
         const contractFunction = async (
           gasPrice
         ): Promise<TransactionReceipt> => {
@@ -148,19 +152,22 @@ export class TransactionBatchSubmitter extends BatchSubmitter {
             nonce,
             contractAddr: this.chainContract.address,
           })
-          const tx = await this.chainContract.appendQueueBatch(99999999, {
-            nonce,
-            gasPrice,
-          })
+          const txResponse = await this.chainContract.appendQueueBatch(
+            99999999,
+            {
+              nonce,
+              gasPrice,
+            }
+          )
           this.logger.info('Submitted appendQueueBatch transaction', {
-            txHash: tx.hash,
-            from: tx.from,
+            txHash: txResponse.hash,
+            from: txResponse.from,
           })
           this.logger.debug('appendQueueBatch transaction data', {
-            data: tx.data,
+            data: txResponse.data,
           })
           return this.signer.provider.waitForTransaction(
-            tx.hash,
+            txResponse.hash,
             this.numConfirmations
           )
         }
@@ -252,25 +259,30 @@ export class TransactionBatchSubmitter extends BatchSubmitter {
     })
 
     const nonce = await this.signer.getTransactionCount()
+    // Generate the transaction we will repeatedly submit
+    const tx = await this.chainContract.customPopulateTransaction.appendSequencerBatch(
+      batchParams
+    )
     const contractFunction = async (gasPrice): Promise<TransactionReceipt> => {
       this.logger.info('Submitting appendSequencerBatch transaction', {
         gasPrice,
         nonce,
         contractAddr: this.chainContract.address,
       })
-      const tx = await this.chainContract.appendSequencerBatch(batchParams, {
+      const txResponse = await this.signer.sendTransaction({
+        ...tx,
         nonce,
         gasPrice,
       })
       this.logger.info('Submitted appendSequencerBatch transaction', {
-        txHash: tx.hash,
-        from: tx.from,
+        txHash: txResponse.hash,
+        from: txResponse.from,
       })
       this.logger.debug('appendSequencerBatch transaction data', {
-        data: tx.data,
+        data: txResponse.data,
       })
       return this.signer.provider.waitForTransaction(
-        tx.hash,
+        txResponse.hash,
         this.numConfirmations
       )
     }
